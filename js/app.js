c0c65cd15a78b64757d76432632e1ae9d760e541
Anna <kostechuk.anna@gmail.com>
2019-04-21 21:06:38 +0800

    Change to MVC

--- js/app.js
+++ js/app.js
@@ -1,162 +1,166 @@
-let button = document.getElementById("button");
-let row, column, block, newSeat, i, j, z;
-let table, tr, td;
-
-function transformInputStringToArray(string) {
-  string = string.replace(/\s/g, ''); //remove extra spaces
-  string = string.substring(2, string.length-2); //remove first and last 2 symbol
-  //transform string to array
-  let array = string.split("],[").map(function (x) {
-    return x.split(",");
-  });
-  //replace strings to numbers
-  for(i = 0; i < array.length; i++){
-    for(j = 0; j < array[i].length; j++){
-      array[i][j] = parseInt(array[i][j]);
-    }
-  };
-  return array;
-}
-
-function clearFromDOM(element){
-  while (element.firstChild) {
-      element.removeChild(element.firstChild);
+function Seat(block, row, column, classSeat, passenger)
+  {
+    this.block = block;
+    this.row = row;
+    this.column = column;
+    this.classSeat = classSeat;
+    this.passenger = passenger;
   }
-}
 
-function addBackgroundImg() {
-  seats.style.background = 'url(\'img/img.png\') no-repeat center top';
-  seats.style.backgroundSize = 'contain';
-}
-
-function Seat (block, row, column, classSeat, passenger)
-{
-  this.block = block;
-  this.row = row;
-  this.column = column;
-  this.classSeat = classSeat;
-  this.passenger = passenger;
-}
-
-function mySort(key) {
-  return function(a, b) {
-    const varA = a[key];
-    const varB = b[key];
-    return ((varA < varB) ? -1 : ((varA > varB) ? 1 : 0));
-  }
-}
-
-function createTableResults (arrInput, arrResult) {
-  for(i=0; i<arrInput.length; i++){
-    table = document.createElement('table');
-    table.setAttribute('class', 'table'+(i+1));
-
-    for(j=0; j<arrInput[i][1]; j++) {
-      tr = document.createElement('tr');
-      tr.setAttribute('class', 'tr'+(j+1));
-      for(z=0; z<arrResult.length; z++) {
-        if(arrResult[z].block===i+1 && arrResult[z].column===j+1) {
-          td = document.createElement('td');
-          td.setAttribute('class', 'class'+arrResult[z].classSeat);
-          if(isNaN(arrResult[z].passenger)===false) {
-            td.innerText=arrResult[z].passenger;
-          }
-          else{
-            td.innerText="";
-          }
-          tr.appendChild(td);
-        }
-      }
-      table.appendChild(tr);
-    }
-    seats.appendChild(table);
-  }
-}
-
-button.addEventListener("click", function() {
+var controller = {
+  init: function() {
+    let button = document.getElementById("button");
+    let seats=document.getElementById("seats");
+    let row, column, block, newSeat, i, j, z;
+    let table, tr, td;
+    let result = [];
+    button.addEventListener("click", function() {
       let queue = document.getElementById('queue').value;
       let stringRowsColumns = document.getElementById("rowsColumns").value;
-      let seats=document.getElementById("seats");
-      let result = [];
+      let inputArrayRowsColumns = controller.transformInputStringToArray(stringRowsColumns);
+      result = [];
 
+      view.clearFromDOM(seats);   // removing old result
+      let check = controller.checkInput(inputArrayRowsColumns, queue);
+      if (check===false) {return false};
+      view.addBackgroundImg();
+      controller.sortingSeats(inputArrayRowsColumns, result);
+      result.sort(controller.mySort('column'));
+      result.sort(controller.mySort('classSeat'));
+      controller.passengerSeating(result, queue);
+      result.sort(controller.mySort('row'));
+      result.sort(controller.mySort('column'));
+      result.sort(controller.mySort('block'));
+      view.createTableResults(inputArrayRowsColumns, result);
+    });
+  },
 
-// removing old result
-      clearFromDOM(seats);
+  transformInputStringToArray: function(string) {
+    string = string.replace(/\s/g, ''); //remove extra spaces
+    string = string.substring(2, string.length-2); //remove first and last 2 symbol
 
-// image airplane in div with results
-      addBackgroundImg();
+    let array = string.split("],[").map(function (x) { //transform string to array
+      return x.split(",");
+    });
 
-// transform input string to array
-      let inputArrayRowsColumns = transformInputStringToArray(stringRowsColumns);
-
-// check input
-      if (inputArrayRowsColumns.length>8) {
-        alert('Too many sections with the rows and columns!');
-        document.getElementById("rowsColumns").focus();
-        return false;
+    for(i = 0; i < array.length; i++){ //replace strings to numbers
+      for(j = 0; j < array[i].length; j++){
+        array[i][j] = parseInt(array[i][j]);
       }
-      for(i = 0; i < inputArrayRowsColumns.length; i++){
-        for(j = 0; j < inputArrayRowsColumns[i].length; j++){
-          if(inputArrayRowsColumns[i][j]<1 || Number.isNaN(inputArrayRowsColumns[i][j])) {
-            alert('The rows and columns must be more than 0!');
-            document.getElementById("rowsColumns").focus();
-            return false;
+    };
+    return array;
+  },
+
+  mySort: function (key) {
+    return function(a, b) {
+      const varA = a[key];
+      const varB = b[key];
+      return ((varA < varB) ? -1 : ((varA > varB) ? 1 : 0));
+    }
+  },
+
+  checkInput: function(arrayRowsColumns, que) {
+    if (arrayRowsColumns.length>8) {
+      alert('Too many sections with the rows and columns!');
+      document.getElementById("rowsColumns").focus();
+      return false;
+    }
+    for(i = 0; i < arrayRowsColumns.length; i++){
+      for(j = 0; j < arrayRowsColumns[i].length; j++){
+        if(arrayRowsColumns[i][j]<1 || Number.isNaN(arrayRowsColumns[i][j])) {
+          alert('The rows and columns must be more than 0!');
+          document.getElementById("rowsColumns").focus();
+          return false;
+        }
+      }
+    }
+    if (que<1 || !que.match('^[0-9]+$')) {
+      alert('Incorrect input!');
+      document.getElementById("queue").focus();
+      return false;
+    }
+  },
+
+  sortingSeats: function(inputArray, resultArr) {
+    for(block = 1; block <= inputArray.length; block++){
+      for(column = 1; column <= inputArray[block-1][0]; column++){
+        for(row = 1; row <= inputArray[block-1][1]; row++){
+          if(block === 1 && column === 1 && inputArray[block-1][0]>1) {
+            newSeat = new Seat(block, column, row, 2);
+            resultArr.push(newSeat);
+          }
+          else if(block === inputArray.length
+            && column === inputArray[block-1][0]
+            && inputArray[block-1][0]>1){
+            newSeat = new Seat(block, column, row, 2);
+            resultArr.push(newSeat);
+          }
+          else if(column === 1 || column === (inputArray[block-1][0])) {
+            newSeat = new Seat(block, column, row, 1);
+            resultArr.push(newSeat);
+          }
+          else {
+            newSeat = new Seat(block, column, row, 3);
+            resultArr.push(newSeat);
           }
         }
       }
-      if (queue<1 || !queue.match('^[0-9]+$')) {
-        alert('Incorrect input!');
-        document.getElementById("queue").focus();
-        return false;
-      }
+    }
+  },
 
-// sorting seats
-      for(block = 1; block <= inputArrayRowsColumns.length; block++){
-        for(column = 1; column <= inputArrayRowsColumns[block-1][0]; column++){
-          for(row = 1; row <= inputArrayRowsColumns[block-1][1]; row++){
-            if(block === 1 && column === 1 && inputArrayRowsColumns[block-1][0]>1) {
-              newSeat = new Seat(block, column, row, 2);
-              result.push(newSeat);
+  passengerSeating: function(res, que) {
+    if(res.length<que) {
+      alert("Only the first "+res.length+" passengers will be able to fly away.");
+      for(i=0; i<res.length; i++){
+        res[i].passenger = i+1;
+      }
+    }
+    else {
+      for(i=0; i<que; i++){
+        res[i].passenger = i+1;
+      }
+    }
+  }
+};
+
+var view = {
+  clearFromDOM: function(element){
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  },
+
+  addBackgroundImg: function() {
+      seats.style.background = 'url(\'img/img.png\') no-repeat center top';
+      seats.style.backgroundSize = 'contain';
+  },
+
+  createTableResults: function(arrInput, arrResult) {
+    for(i=0; i<arrInput.length; i++){
+      table = document.createElement('table');
+      table.setAttribute('class', 'table'+(i+1));
+
+      for(j=0; j<arrInput[i][1]; j++) {
+        tr = document.createElement('tr');
+        tr.setAttribute('class', 'tr'+(j+1));
+        for(z=0; z<arrResult.length; z++) {
+          if(arrResult[z].block===i+1 && arrResult[z].column===j+1) {
+            td = document.createElement('td');
+            td.setAttribute('class', 'class'+arrResult[z].classSeat);
+            if(isNaN(arrResult[z].passenger)===false) {
+              td.innerText=arrResult[z].passenger;
             }
-            else if(block === inputArrayRowsColumns.length
-              && column === inputArrayRowsColumns[block-1][0]
-              && inputArrayRowsColumns[block-1][0]>1){
-              newSeat = new Seat(block, column, row, 2);
-              result.push(newSeat);
+            else{
+              td.innerText="";
             }
-            else if(column === 1 || column === (inputArrayRowsColumns[block-1][0])) {
-              newSeat = new Seat(block, column, row, 1);
-              result.push(newSeat);
-            }
-            else {
-              newSeat = new Seat(block, column, row, 3);
-              result.push(newSeat);
-            }
+            tr.appendChild(td);
           }
         }
+        table.appendChild(tr);
       }
+      seats.appendChild(table);
+    }
+  }
+}
 
-      result.sort(mySort('column'));
-      result.sort(mySort('classSeat'));
-
-//passenger seating
-      if(result.length<queue) {
-        alert("Only the first "+result.length+" passengers will be able to fly away.");
-        for(i=0; i<result.length; i++){
-          result[i].passenger = i+1;
-        }
-      }
-      else {
-        for(i=0; i<queue; i++){
-          result[i].passenger = i+1;
-        }
-      }
-
-//output of the result
-      result.sort(mySort('row'));
-      result.sort(mySort('column'));
-      result.sort(mySort('block'));
-
-      createTableResults(inputArrayRowsColumns, result)
-
-});
+controller.init();
